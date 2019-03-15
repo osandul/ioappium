4ed08bf3388a39ad604207e38296a2974aa70fa5
osandul <sashka95.13@gmail.com>
2019-03-15 17:03:10 +0200

    2 tests for signIn

--- src/test/java/BookingSignIn.java
+++ src/test/java/BookingSignIn.java
@@ -3,6 +3,8 @@ import PageObjects.SignInPage;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -10,6 +12,9 @@ public class BookingSignIn {
     AppiumDriver driver;
 
     SignInPage signInPage = new SignInPage ( );
+
+    public String successful_signIn = "Check your inbox for an email. Use the provided link to activate your account.";
+    public String email_exists_inDB = "Please enter a different email address, this one has been saved to an existing profile.";
 
 
     @BeforeTest
@@ -19,7 +24,30 @@ public class BookingSignIn {
 
     @Test
 
-    public void basicTest() {
+    public void signInTest() {
+        WebDriverWait wait = new WebDriverWait ( driver, 10 );
+
+        wait.until ( ExpectedConditions.visibilityOfElementLocated ( signInPage.signIn_start_button ) ).click ( );
+        wait.until ( ExpectedConditions.visibilityOfElementLocated ( signInPage.signIn_email_field ) ).click ( );
+        driver.findElement ( signInPage.signIn_email_input ).sendKeys ( "oleksandria.sanddul13@gmail.com" );
+        wait.until ( ExpectedConditions.visibilityOfElementLocated ( signInPage.signIn_password_field ) ).click ( );
+        driver.findElement ( signInPage.signIn_password_input ).sendKeys ( "1234abcd" );
+        driver.findElement ( signInPage.signIn_submit_button ).click ( );
+        wait.until ( ExpectedConditions.visibilityOfElementLocated ( signInPage.signIn_dialogue_checkmailbox ) );
+        String signIn_message = driver.findElement ( signInPage.signIn_dialogue_checkmailbox ).getAttribute ( "text" );
+
+        Assert.assertEquals ( signIn_message, successful_signIn );
+
+        //       System.out.println ( "The registration was successfully completed" );
+
+        //      signIn_output = false && signIn_message.equals ( email_exists_inDB );
+        //      System.out.println ("Error. This email already exists in base");
+
+
+    }
+
+    @Test
+    public void emailExistsTest(){
         WebDriverWait wait = new WebDriverWait ( driver, 10 );
 
         wait.until ( ExpectedConditions.visibilityOfElementLocated ( signInPage.signIn_start_button ) ).click ( );
@@ -28,6 +56,18 @@ public class BookingSignIn {
         wait.until ( ExpectedConditions.visibilityOfElementLocated ( signInPage.signIn_password_field ) ).click ( );
         driver.findElement ( signInPage.signIn_password_input ).sendKeys ( "1234abcd" );
         driver.findElement ( signInPage.signIn_submit_button ).click ( );
+        wait.until ( ExpectedConditions.visibilityOfElementLocated ( signInPage.signIn_dialogue_checkmailbox ) );
+        String signIn_message = driver.findElement ( signInPage.signIn_dialogue_checkmailbox ).getAttribute ( "text" );
+
+        Assert.assertEquals ( signIn_message, email_exists_inDB );
     }
+
+    @AfterTest
+
+    public void quitDriver() {
+        AppiumDriverInit.closeDriver ();
+    }
+    
+
 }
 
